c3407edf633ef1bb96447ec70271045bffb4e3ce
Valisher Muminov <valishaermuminov@gmail.com>
2023-05-26 20:02:56 +0900

    loyihani express qismini shakllantirib olish

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+
+//MongoDB connect
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+// 1: Kirish code
+// express ga kirib kelayotgan kodlar
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://valishermuminov:iBBmiNi2Hvc8hFMi@cluster0.hiakcdu.mongodb.net/Papays?retryWrites=true&w=majority";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection Mongodb");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = process.env.PORT || 3010;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);

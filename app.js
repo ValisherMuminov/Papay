2dca7f1f7ce0b44212f136d8ff3f8ecfe187f1aa
Valisher Muminov <valishaermuminov@gmail.com>
2023-05-30 16:26:21 +0900

    feat: develop signup business logic

--- app.js
+++ app.js
@@ -12,11 +12,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         cookie: {
+//             maxAge: 1000 * 60 * 30, // for 30 minutes
+//         },
+//         store: store,
+//         resave: true,
+//         saveUnitialized: true,
+//     })
+// );
 // 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
+// app.use("/resto", router_bssr)
 app.use("/", router);
+
 
 module.exports = app;
--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,12 +1,19 @@
+const Member = require("../models/Member");
 let memberController = module.exports;
 
-memberController.home = (req, res) => {
-    console.log("GET controller.home");
-    res.send("Home sahifadasiz");
-};
-memberController.signup = (req, res) => {
-    console.log("POST controller.signup");
-    res.send("Signup sahifadasiz");
+
+memberController.signup = async (req, res) => {
+    try {
+        console.log('POST: cont/signup');
+        const data = req.body;
+        const member = new Member();
+        const new_member = await member.signupData(data);
+
+        res.send("done")
+    } catch (err) {
+    console.log(`ERROR: cont/signup, ${err.message}`);
+        
+    }
 };
 memberController.login = (req, res) => {
     console.log("POST controller.login");
@@ -16,3 +23,4 @@ memberController.logout = (req, res) => {
     console.log("GET controller.logout");
     res.send("Logout sahifadasiz");
 };
+

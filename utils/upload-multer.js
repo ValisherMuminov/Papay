a4d5f3f767c233c39fb036c95ecc8a099a9bb84b
Valisher Muminov <valishaermuminov@gmail.com>
2023-06-04 22:26:44 +0900

    fix: modify image uploader utils

--- utils/upload-multer.js
+++ utils/upload-multer.js
@@ -1,10 +1,12 @@
 const path = require("path");
-const multer = require("multer");
+const multer = require('multer');
 const uuid = require("uuid");
 
-const product_storage = multer.diskStorage({
+/** MULTER IMAGE UPLOADER */
+function getTargetImageStorage(address) {
+  return multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads/products");
+    cb(null, `./uploads/${address}`);
   },
   filename: function (req, file, cb) {
     console.log(file);
@@ -13,5 +15,23 @@ const product_storage = multer.diskStorage({
     cb(null, random_name);
   },
 });
+}
+const makeUploader = (address) => {
+  const storage = getTargetImageStorage(address);
+  return multer({storage: storage})
+}
+module.exports = makeUploader;
 
-module.exports.uploadProductImage = multer({ storage: product_storage });
+// const product_storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads/products");
+//   },
+//   filename: function (req, file, cb) {
+//     console.log(file);
+//     const extension = path.parse(file.originalname).ext;
+//     const random_name = uuid.v4() + extension;
+//     cb(null, random_name);
+//   },
+// });
+
+// module.exports.uploadProductImage = multer({ storage: product_storage });

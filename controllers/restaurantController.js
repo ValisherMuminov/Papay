b9107578990eb735d0d650e281acdf5090d042b5
Valisher Muminov <valishaermuminov@gmail.com>
2023-06-24 07:31:50 +0900

    fix: modify sign-up link on adminka

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -119,3 +119,28 @@ restaurantController.checkSessions = (req, res) => {
     res.json({ state: "fail", message: "You are not authenticated" });
   }
 };
+
+restaurantController.validateAdmin = (req, res, next) => {
+  if (req.session?.member?.mb_type === "ADMIN") {
+    req.member = req.session.member;
+    next();
+  } else {
+    const html = `<script>
+                    alert('Admin page: Permission denied!');
+                    window.location.replace('/resto');
+                  </script>`;
+    res.end(html);
+  }
+};
+
+restaurantController.getAllRestaurants = (req, res) => {
+  try {
+    console.log("GET cont/getAllRstaurants");
+    // todo: home retasurantlarni dbdan chaqiramiz
+
+    res.render("all-restaurants");
+  } catch (err) {
+     console.log(`ERROR: cont/logout, ${err.message}`);
+     res.json({ state: "fail", message: err.message });
+  }
+}
